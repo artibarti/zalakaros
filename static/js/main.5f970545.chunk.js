fd600689841a5ab23c2ed3745a783e5de20c0390
Artur Safrany <safranyartur1995@gmail.com>
2021-08-24 22:18:35 +0200

    Updates

--- static/js/main.5f970545.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpzalakaros=this.webpackJsonpzalakaros||[]).push([[0],{9:function(e,t,a){"use strict";a.r(t);var r=a(1),n=a.n(r),s=a(3),c=a.n(s),o=a(0);var i=function(){return Object(o.jsx)("div",{children:"Hello Zalakaros"})};c.a.render(Object(o.jsx)(n.a.StrictMode,{children:Object(o.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.5f970545.chunk.js.map
